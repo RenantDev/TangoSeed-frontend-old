d0d8e7ba30917110023ce01e93520ac0910be54f
Elias Faiçal <eliasfaical@gmail.com>
2017-12-22 16:02:48 -0200

    Ajsustes

--- src/app/views/appviews/login.service.ts
+++ src/app/views/appviews/login.service.ts
@@ -1,50 +1,48 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Login} from './login';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Response } from '@angular/http';
+import { Login } from './login';
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
 export class LoginService {
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    private oauthUrl = 'http://api.tangoseed.dev/oauth/token';
+  private oauthUrl = 'http://api.tangoseed.com/oauth/token';
 
+  getAccessToken(login: Login) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
 
-    getAccessToken(login: Login) {
+    const postData = {
+      username: login.username,
+      password: login.password,
+      client_id: 2,
+      client_secret: 'cbA2pSWnY675qyPlgGn73l9wHrcQeaBXv337Pjnx',
+      grant_type: 'password',
+      scope: ''
+    };
 
-        const headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        });
+    return this.http.post(this.oauthUrl, JSON.stringify(postData), {
+      headers: headers
+    });
+  }
 
-        const postData = {
-            username: login.username,
-            password: login.password,
-            client_id: 2,
-            client_secret: 'IpQ4Bdj2Q6idXapi9QhFhaV6Z1kMiRokkNpBgvEd',
-            grant_type: 'password',
-            scope: ''
-        };
+  getUsers(accessToken: string): Observable<Login[]> {
+    const headers = new HttpHeaders({
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ' + accessToken,
+    });
 
-        return this.http.post(this.oauthUrl, JSON.stringify(postData), {
-            headers: headers
-        });
-
-    }
-
-    // getUsers(accessToken: string): Observable<Usuario[]> {
-    //
-    //     const headers = new Headers({
-    //         'Accept': 'application/json',
-    //         'Authorization': 'Bearer ' + accessToken,
-    //     });
-    //
-    //     return this.http.get(this.usersUrl, {
-    //         headers: headers
-    //     })
-    //         .map((res: Response) => res.json())
-    //         .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    // }
+    return this.http.get(this.oauthUrl, {
+      headers: headers
+    })
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
 }
